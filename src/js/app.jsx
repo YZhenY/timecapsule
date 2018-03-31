c8ae6212e0f014f6e18ba682d9db2905cf2df2b9
YZhenY <fuz3zy@gmail.com>
2018-03-31 15:21:54 -0700

    transfered initialization of web3 app

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button, Input } from 'semantic-ui-react'
 
 window.AppRoot = {
     web3Provider: null,
@@ -17,20 +18,33 @@ window.AppRoot = {
 
 class App extends React.Component {
   constructor(props) {
-      super(props);      
-      this.state = {
-        web3Provider: null,
-        contracts: {},
-      }
+    super(props);      
+    this.state = {
+      web3Provider: null,
+      contracts: {},
+    }
+    this.initWeb3();
+  }
+
+  initWeb3() {
+    if (typeof web3 !== 'undefined') {
+      this.setState({web3Provider:web3.currentProvider});
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      this.setState({web3Provider: new Web3.providers.HttpProvider('http://localhost:7545')}) 
+    }
+    web3 = new Web3(App.web3Provider);
   }
 
   //sticky items can be configured later
   render () {
       return (
         <div>
-          <input type='text' />
+          <Input className='textInput' type='text' />
+          <Button>Mail Message </Button>
         </div>
       )
   }
 }
-export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
